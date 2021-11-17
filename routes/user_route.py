e1e17148e35b7e381f0e5a9493b401d7b0935882
Brian Vo <vnbaotran@gmail.com>
2021-11-17 13:13:06 -0800

    add endpoint add/remove member, and a test function to find_all_user

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,7 +1,30 @@
 from flask import Blueprint, jsonify, make_response
 from flask.globals import request
+import json
+from bson import json_util
 from .database import get_db
 user_routes = Blueprint("user", __name__)
+
+
+@user_routes.route("/")
+def find_all_user():
+    # for testing only
+    db = get_db()
+    # member must be alone and not have any member
+    # added_members = db.users.find_one(
+    #     {"username": "username"}, {"members": True})
+
+    # print(len([]))
+    # print(len(added_members.get("members")))
+
+    users = list(db.users.find({}))
+
+    users_dicts = [doc for doc in users]
+
+    # serialize to json string
+    users_json_string = json.dumps(users_dicts, default=json_util.default)
+    return jsonify({"users": json.loads(users_json_string)})
+    # return jsonify({"users": json_util.dumps(users)})
 
 
 @user_routes.route("/login", methods=['POST'])
@@ -46,3 +69,74 @@ def signup():
     }
     user_id = db.users.insert_one(user).inserted_id
     return jsonify({"id": str(user_id)})
+
+
+@user_routes.route("/add-member", methods=["POST"])
+def add_member():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    member = data.get("member")
+    if (member is not None and username is not None and password):
+        db = get_db()
+        user = db.users.find_one({
+            "username": username,
+            "password": password,
+            "is_owner": True})
+        if (user is None):
+            return make_response(jsonify({"message": "you dont have permission to add member"}), 403)
+
+        # member must be alone and not have any member
+        added_member = db.users.find_one(
+            {"username": "username"}, {"members": True})
+        if (added_member.get("members") is not None and len(added_member.get("members")) > 0):
+            return make_response(jsonify({"message": "Can not add member to your household, member must be alone."}), 403)
+
+        # add member to household
+        db.users.update_one(
+            {"username": username},
+            {"$push": {"members": member}},
+            upsert=True
+        )
+
+        # change member owner to false, they are not on their own anymore
+        db.users.update_one(
+            {"username": member},
+            {"$set": {"is_owner": False}}
+        )
+
+        return jsonify({"message": "member was added to household"})
+    else:
+        return make_response(jsonify({"message": "Invalid request"}), 403)
+
+
+@ user_routes.route("remove-member", methods=["POST"])
+def remove_member():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    member = data.get("member")
+    if (member is not None and username is not None and password):
+        db = get_db()
+        user = db.users.find_one({
+            "username": username,
+            "password": password,
+            "is_owner": True})
+        if (user is None):
+            return make_response(jsonify({"message": "you dont have permission to remove member"}), 403)
+
+        # remove from household
+        db.users.update_one(
+            {"username": username},
+            {"$pull": {"members": member}}
+        )
+
+        # set the removed member to the owner on their own
+        db.users.update_one(
+            {"username": username},
+            {"$set": {"is_owner": True}}
+        )
+
+        return jsonify({"message": "member was removed from household"})
+    else:
+        return make_response(jsonify({"message": "Invalid request"}), 403)
